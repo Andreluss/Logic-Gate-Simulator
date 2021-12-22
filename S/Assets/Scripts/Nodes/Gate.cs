7d39e92581202eff8c742bc6abe2d5e6ed79ca7c
Andrzej Jabłoński <64368904+Andreluss@users.noreply.github.com>
2021-12-22 22:18:35 +0100

    BL pt.5.7

--- S/Assets/Scripts/Nodes/Gate.cs
+++ S/Assets/Scripts/Nodes/Gate.cs
@@ -21,95 +21,6 @@ using UnityEngine;
     //...
 }
 
-[Serializable] public enum NodeType
-{
-    Input, Output, AND, NOT, Split, ComplexGate
-}
-
-//This class requires AppSaveData.gateTemplates
-[Serializable]
-public class GateTemplate
-{
-    public NodeType NodeType;
-    public int inCnt, outCnt;
-    public string defaultName;
-    public int N, M;//number of verticies(nodes) and edges
-    public RenderProperties renderProperties;
-    public int[] TemplateIDsForEachNode;
-    public Pair<Pair<int, int>, Pair<int, int>>[] edges;
-    //(Source_Node_ID, OutID), (Destination_Node_Id, InID)
-    public Node BuildNodeFromTemplate(bool hidden = true, Vector2? where = null)
-    {
-        Node node;
-        if(NodeType != NodeType.ComplexGate)
-        {
-            switch (NodeType)
-            {
-                case NodeType.Input:
-                    node = new InputNode(hidden); // #### tutaj dodac wiecej typow inputu
-                    break;
-                case NodeType.Output:
-                    node = new OutputNode(hidden);
-                    break;
-                case NodeType.AND:
-                    node = new GateAND(hidden);
-                    break;
-                case NodeType.NOT:
-                    node = new GateNOT(hidden);
-                    break;
-                case NodeType.Split:
-                    node = new Split(hidden);
-                    break;
-                default:
-                    throw new Exception("unrecognized gate type");
-            }
-        }
-        else
-        {
-            var gate = new Gate(inCnt, outCnt, defaultName, hidden);
-            //tu sie dzieje główna rekurencyjna część:
-            Node[] IDtoNode = new Node[N];
-            for (int i = 0; i < N; i++)
-            {
-                //TODO: add cheks for size
-                int id = TemplateIDsForEachNode[i];
-                GateTemplate template = AppSaveData.GateTemplates[id];
-                IDtoNode[i] = template.BuildNodeFromTemplate(); //lessgo
-                //TODO: save internal input/output sockets
-                
-                if(template.NodeType == NodeType.Input)
-                {
-                    var inputNode = (InputNode)IDtoNode[i];
-                    gate.internalIns.Add(inputNode);
-                }
-                else if(template.NodeType == NodeType.Output)
-                {
-                    var outputNode = (OutputNode)IDtoNode[i];
-                    gate.internalOuts.Add(outputNode);
-
-                }
-            }
-
-            foreach (var edge in edges)
-            {
-                //TODO: checks here as well
-                Node A = IDtoNode[edge.st.st],
-                     B = IDtoNode[edge.nd.st];
-                int outIdx = edge.st.nd,
-                    inpIdx = edge.nd.nd;
-
-                A.ConnectTo(outIdx, B, inpIdx);
-            }
-
-            node = gate;
-        }
-
-        if(where != null)
-            node.Position = (Vector2)where;
-
-        return node;
-    }
-}
 
 public class Gate : Node
 {
@@ -130,7 +41,7 @@ public class Gate : Node
             throw new Exception("invals and internalIns have incompatible sizes");
         for (int i = 0; i < inVals.Length; i++)
         {
-            internalIns[i].SetValue(inVals[i]); // ####
+            internalIns[i].SetValue(inVals[i]); 
         }
         
         NodeSearch.RunSearchAndCalculateAllNodes(internalIns, internalOuts);
---
+++ S/Assets/Scripts/Nodes/GateTemplate.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System;
+
+[Serializable]
+public enum NodeType
+{
+    Input, Output, AND, NOT, Split, ComplexGate
+}
+
+//This class requires AppSaveData.gateTemplates
+[Serializable]
+public class GateTemplate
+{
+    public NodeType NodeType;
+    public int inCnt, outCnt;
+    public string defaultName;
+    public int N, M;//number of verticies(nodes) and edges
+    public RenderProperties renderProperties;
+    public int[] TemplateIDsForEachNode;
+    public Pair<Pair<int, int>, Pair<int, int>>[] edges;
+    //(Source_Node_ID, OutID), (Destination_Node_Id, InID)
+    public Node BuildNodeFromTemplate(bool hidden = true, Vector2? where = null)
+    {
+        Node node;
+        if (NodeType != NodeType.ComplexGate)
+        {
+            switch (NodeType)
+            {
+                case NodeType.Input:
+                    node = new InputNode(hidden); // #### tutaj dodac wiecej typow inputu
+                    break;
+                case NodeType.Output:
+                    node = new OutputNode(hidden);
+                    break;
+                case NodeType.AND:
+                    node = new GateAND(hidden);
+                    break;
+                case NodeType.NOT:
+                    node = new GateNOT(hidden);
+                    break;
+                case NodeType.Split:
+                    node = new Split(hidden);
+                    break;
+                default:
+                    throw new Exception("unrecognized gate type");
+            }
+        }
+        else
+        {
+            var gate = new Gate(inCnt, outCnt, defaultName, hidden);
+            //tu sie dzieje główna rekurencyjna część:
+            Node[] IDtoNode = new Node[N];
+            for (int i = 0; i < N; i++)
+            {
+                //TODO: add cheks for size
+                int id = TemplateIDsForEachNode[i];
+                GateTemplate template = AppSaveData.GateTemplates[id];
+                IDtoNode[i] = template.BuildNodeFromTemplate(); //lessgo
+                                                                //TODO: save internal input/output sockets
+
+                if (template.NodeType == NodeType.Input)
+                {
+                    var inputNode = (InputNode)IDtoNode[i];
+                    gate.internalIns.Add(inputNode);
+                }
+                else if (template.NodeType == NodeType.Output)
+                {
+                    var outputNode = (OutputNode)IDtoNode[i];
+                    gate.internalOuts.Add(outputNode);
+
+                }
+            }
+
+            foreach (var edge in edges)
+            {
+                //TODO: checks here as well
+                Node A = IDtoNode[edge.st.st],
+                     B = IDtoNode[edge.nd.st];
+                int outIdx = edge.st.nd,
+                    inpIdx = edge.nd.nd;
+
+                A.ConnectTo(outIdx, B, inpIdx);
+            }
+
+            node = gate;
+        }
+
+        if (where != null)
+            node.Position = (Vector2)where;
+
+        return node;
+    }
+}
