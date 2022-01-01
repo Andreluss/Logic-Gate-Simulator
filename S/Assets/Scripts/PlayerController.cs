2d2c3d3f61a67853ca590f2a3a8f416952fcce9f
Andrzej Jabłoński <64368904+Andreluss@users.noreply.github.com>
2022-01-01 15:35:12 +0100

    Update PlayerController.cs

--- S/Assets/Scripts/PlayerController.cs
+++ S/Assets/Scripts/PlayerController.cs
@@ -4,17 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private Camera m_Camera;
     StateMachine StateMachine;
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_Camera = Camera.main;
+    }
+
+    private GameObject GetObjectUnderMouse()
+    {
+        var origin = m_Camera.ScreenToWorldPoint(Input.mousePosition);
+        var hit = Physics2D.Raycast(origin, Vector2.zero);
+        if (hit.collider != null) 
+            Debug.Log(hit.collider.gameObject);
+        return hit.collider.gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("clicked " + GetObjectUnderMouse().name);
+        }
     }
 
     
