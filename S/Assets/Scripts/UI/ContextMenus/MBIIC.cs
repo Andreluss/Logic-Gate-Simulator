7fdaaf191c22ef76f986cc5997c2c0ce7f8d68f7
Andrzej Jabłoński <64368904+Andreluss@users.noreply.github.com>
2022-03-07 21:22:56 +0100

    pewnie ostatni commit

--- S/Assets/Scripts/UI/ContextMenus/MBIIC.cs
+++ S/Assets/Scripts/UI/ContextMenus/MBIIC.cs
@@ -8,6 +8,9 @@ public class MBIIC : ItemController
     {
         base.Start();
 
+        if(PlayerController.Instance.Mode == PlayerController.GameMode.Edit)
+            contextMenuItems.RemoveAt(0);
+
         var ctrl = (rClickableObj as MBICollision).node;
         contextMenuItems.Add(new ContextMenuItem("Switch signed/unsigned", sampleButton,
             () => (ctrl as MultibitControllerInput).Signed = !(ctrl as MultibitControllerInput).Signed));
--- S/Assets/Scripts/UI/ContextMenus/MBOIC.cs
+++ S/Assets/Scripts/UI/ContextMenus/MBOIC.cs
@@ -8,6 +8,9 @@ public class MBOIC : ItemController
     {
         base.Start();
 
+        if (PlayerController.Instance.Mode == PlayerController.GameMode.Edit)
+            contextMenuItems.RemoveAt(0);
+
         var ctrl = (rClickableObj as MBOCollision).node;
         contextMenuItems.Add(new ContextMenuItem("Switch Signed/Unsigned", sampleButton,
             () => (ctrl as MultibitControllerOutput).Signed = !(ctrl as MultibitControllerOutput).Signed));
